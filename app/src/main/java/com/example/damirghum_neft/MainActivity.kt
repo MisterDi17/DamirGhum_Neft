6edce52881b1a0fcebf26d58076116de54c779e9
Moroh <dimastermorozow637@gmail.com>
2024-11-21 23:40:12 +0500

    Готово

---
+++ app/src/main/java/com/example/damirghum_neft/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.damirghum_neft
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        setContentView(R.layout.activity_main)
+    }
+}
